da21db1ccb9b95d24cdff1977b4b964b12987c13
Cristian Tulbu <cristian.tulbu@gmail.com>
2022-03-16 19:56:44 +0100

    Inserted Bombs

--- script.js
+++ script.js
@@ -16,22 +16,47 @@ button.addEventListener('click', function () {
     difficulty = parseInt(document.querySelector('#range').value);
     createGrid(); //Create a new grid
 
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Function for GRID creation
 function createGrid() {
+    let bombs = generateBombs(1, difficulty, 16);
+    console.log(bombs);
     for (let i = 0; i < difficulty; i++) {
         const cell = createCell();
         cell.innerText = (i + 1);
-
+        cell.setAttribute('id', `square-${i + 1}`);
+        if (bombs.includes(i + 1)) {
+            console.log('c è in posizione', i + 1);
+            cell.classList.add('bomb');
+        }
         cell.addEventListener('click', function () {
             cell.classList.toggle('ct-blue');
         })
         grid.appendChild(cell);
     }
 }
-
-
 
 
 //Function for Cells Creation
@@ -46,3 +71,19 @@ function createCell() {
     }
     return element;
 }
+
+// Generate Bombs
+function generateBombs(min, max, nrBombs) {
+    const range = max - min;
+    let array = [];
+    for (i = 0; i < nrBombs; i++) {
+        number = Math.floor(Math.random() * range + 1);
+        while (array.includes(number)) {
+            number = Math.floor(Math.random() * range + 1);
+        }
+        array.push(parseInt(number));
+    }
+    return array;
+}
+
+
